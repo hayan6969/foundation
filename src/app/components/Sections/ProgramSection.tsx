91ec798d84a1cf42a569fd68a1362276abe08bd6
Suleman Qureshi <143796869+Suleman-Qureshi@users.noreply.github.com>
2025-03-13 17:35:33 +0500

    debugged

--- src/app/components/Sections/ProgramSection.tsx
+++ src/app/components/Sections/ProgramSection.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 function ProgramSection() {
   return (
-    <section className="w-screen min-h-screen flex flex-col items-center gap-8 pt-10 px-20 max-md:px-10 max-sm:px-4">
+    <section className="w-screen flex flex-col items-center gap-8 pt-10 px-20 max-md:px-10 max-sm:px-4">
       <h2 className="text-4xl font-semibold text-[#9E0C39]">Programs</h2>
       <div className="w-full flex gap-4 max-md:flex-col justify-between items-center max-md:hidden">
         <ProgramCard/>
       </div>
       <div className="w-full flex gap-4 max-md:flex-col justify-between items-center md:hidden">
         <div className="flex gap-4 justify-between items-center max-md:justify-center max-sm:flex-col">
-          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[20rem] ">
+          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[18rem] ">
             <div className="h-[7rem] w-full rounded-md textc bg-gray-400 overflow-hidden">
               {" "}
               <Image
@@ -27,12 +27,12 @@ function ProgramSection() {
               Red Orchard Animal Rescue {"(ROAR)"}{" "}
             </h5>
             <div>
-              <Link href='/Roar' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
+              <Link href='/Roar' className="px-4 py-1 border border-[#9E0C39] rounded-sm text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
                 Learn More
               </Link>
             </div>
           </div>
-          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[20rem] ">
+          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[18rem] ">
             <div className="h-[7rem] w-full rounded-md textc bg-gray-400 overflow-hidden">
               {" "}
               <Image
@@ -48,14 +48,14 @@ function ProgramSection() {
             The Warrior Project
             </h5>
             <div>
-              <Link href='/Warrior-Project' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
+              <Link href='/Warrior-Project' className="px-4 py-1 border border-[#9E0C39] rounded-sm text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
                 Learn More
               </Link>
             </div>
           </div>
         </div>
         <div className="flex gap-4 justify-between items-center max-md:justify-center max-sm:flex-col">
-          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[20rem] ">
+          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[18rem] ">
             <div className="h-[7rem] w-full rounded-md textc bg-gray-400 overflow-hidden">
               {" "}
               <Image
@@ -70,12 +70,12 @@ function ProgramSection() {
             P.P.P - Purple Paw Project
             </h5>
             <div>
-              <Link href='/P.P.P' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
+              <Link href='/P.P.P' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] rounded-sm hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
                 Learn More
               </Link>
             </div>
           </div>
-          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[20rem] ">
+          <div className="w-[12rem] flex flex-col p-4 gap-4 text-black items-center justify-between h-[18rem] ">
             <div className="h-[7rem] w-full rounded-md textc bg-gray-400 overflow-hidden">
               {" "}
               <Image
@@ -90,7 +90,7 @@ function ProgramSection() {
             HEAL - Helping Everyone After Loss
             </h5>
             <div>
-              <Link href='/HEAL' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
+              <Link href='/HEAL' className="px-4 py-1 border border-[#9E0C39] text-[#9E0C39] rounded-sm hover:bg-[#9E0C39] hover:text-white duration-200 cursor-pointer">
                 Learn More
               </Link>
             </div>
